7d214b38dc29fac05cc7b6efc789e3bc73a19511
chahe-dridi <chaher.dridi.100@gmail.com>
2026-02-13 06:14:00 +0100

    added api gateway

---
+++ api-gateway/src/main/java/tn/esprit/microservice/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,34 @@
+package tn.esprit.microservice.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                /*.route("reclamation_service", r -> r.path("/reclamations/**")
+                        .uri("lb://reclamation-service"))  // The exact service name in Eureka
+                 */
+                .build();// The exact service name in Eureka
+
+
+    }
+}
